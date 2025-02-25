a2acb515f010b3441bcb939afa31111c37055af6
Sadinal Mufti <166024867+sadinal04@users.noreply.github.com>
2025-02-25 20:00:40 +0700

    Menambahkan Fungsi pangkat dan faktorial ke solution.js

--- solution.js
+++ solution.js
@@ -19,9 +19,25 @@ function bagi(a, b) {
     return a / b;
 }
 
+function pangkat(a, b) {
+    return Math.pow(a, b);
+}
+
+function faktorial(n) {
+    if (n < 0) return undefined;
+    if (n === 0 || n === 1) return 1;
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
 module.exports = {
     tambah,
     kurang,
     kali,
-    bagi
+    bagi,
+    pangkat,
+    faktorial
 };
